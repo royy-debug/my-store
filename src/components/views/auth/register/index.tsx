3ff64f7c4860380f44c45a5fcdf25474c572b40f
royy-debug <royyzain@gmail.com>
2025-04-25 21:48:43 +0700

    auth

--- src/components/views/auth/register/index.tsx
+++ src/components/views/auth/register/index.tsx
@@ -1,20 +1,61 @@
-    import styles from './Register.module.scss';
+    import Link from 'next/link';
+import styles from './Register.module.scss';
+import { useRouter } from 'next/router';
+import { FormEvent } from 'react';
     
     const RegisterView = () => {
+        const {push} = useRouter();
+        const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            const form =event.target as HTMLFormElement
+            const data = {
+                
+                email: form.email.value,
+                fullname: form.fullname.value,
+                phone: form.phone.value,
+                password: form.password.value,
+            }
+            const result = await fetch('/api/user/register', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data),
+
+            })
+            if (result.status === 200) {
+form.reset();
+push('/auth/login');
+            }else{
+                 console.log('error')
+            }
+        }
     return (
         <div className={styles.register}>
             <h1 className={styles.register__title}>Register</h1>
             <div className={styles.register__form}>
-                <form action="">
-                    <div>
-                        <label htmlFor=""></label>
-                        <input type="text" />
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.register__form__item}>
+                        <label htmlFor="email">Email</label>
+                        <input name="email" id="email" type="text" className={styles.register__form__item__input}/>
                     </div>
-                    <button>Register</button>
+                    <div className={styles.register__form__item}>
+                        <label htmlFor="">Fullname</label>
+                        <input name="fullname" id="fullname" type="text" className={styles.register__form__item__input}/>
+                    </div>
+                    <div className={styles.register__form__item}>
+                        <label htmlFor="">Phone</label>
+                        <input name="phone" id="phone" type="number" className={styles.register__form__item__input}/>
+                    </div>
+                    <div className={styles.register__form__item}>
+                        <label htmlFor="">Password</label>
+                        <input name="password" id="password" type="password" className={styles.register__form__item__input}/>
+                    </div>
+                    <button type="submit" className={styles.register__form__button}>Register</button>
                 </form>
             </div>
-            <p>
-                have a account?sign in here
+            <p className={styles.register__link}>
+                have a account?sign in <Link href="/auth/login">Here</Link>
             </p>
         </div>
     );
